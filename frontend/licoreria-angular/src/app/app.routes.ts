8e17bd6dbf9298b2de438cdeb7b63cb5769277a4
Adri <adriana.ovando@ucb.edu.bo>
2023-11-28 21:09:55 -0400

    subiendo creacion del componente nosotros

--- frontend/licoreria-angular/src/app/app.routes.ts
+++ frontend/licoreria-angular/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NosotrosComponent } from './paginas/nosotros/nosotros.component';
+
+export const routes: Routes = [
+    { path: 'nosotros', component:  },
+
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+export class AppRoutingModule { }
